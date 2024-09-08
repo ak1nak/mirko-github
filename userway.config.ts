2bed532eab27a4d714bcf416252fda672edd680c
Serhii Diachenko <sergeycerb@gmail.com>
2024-09-08 16:31:19 +0300

    config

--- userway.config.ts
+++ userway.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@userway/cicd-cli";
 export default {
   organization: "serhii-diachenko-userway-org-toqmw",
-  project: "test-mirko-ready2go-main",
+  project: "mirko-github",
   reportPaths: ["./uw-a11y-reports"],
   token: process.env.USERWAY_TOKEN,
   server: "https://api.qa.userway.dev",
