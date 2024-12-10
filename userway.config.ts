01f7c76add3bc27cab866bdef1c17b5ea239eb17
Serhii Diachenko <sergeycerb@gmail.com>
2024-12-10 16:12:20 +0200

    1

--- userway.config.ts
+++ userway.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "@userway/cicd-core";
 export default {
   organization: "serhii-diachenko-userway-org-toqmw",
   project: "mirko-github",
-  token: "axvnjixeozojpy750bcuekqokernqz9c9nswrwgc",
+  token: "ah33jcpfppmyor4l5aui70nmbniqcakjsr7bkyjk",
   reportPaths: ["./uw-a11y-reports"],
   server: "https://api.dev.userway.dev",
 } satisfies Config;
